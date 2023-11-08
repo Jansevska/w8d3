be5fbcc389208a9468abd034c415f1c471265134
Emili <ejanchevis@gmail.com>
2023-11-08 12:04:13 -1000

    first commit

---
+++ src/area.js
@@ -0,0 +1,10 @@
+// Pure JS here not type checking
+
+// below is not a comment (add after turning on checkJS)
+// this tells TS not to type check this file
+
+// @ts-nocheck
+
+export function squareFootage(length, width){
+    return length*width
+}
